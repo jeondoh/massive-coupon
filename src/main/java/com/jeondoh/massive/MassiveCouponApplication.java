576ae1c7d94a61c580b899d86eab42e357ac93db
jeondoh <jundo1302@gmail.com>
2025-12-01 23:55:25 +0900

    init project

---
+++ src/main/java/com/jeondoh/massive/MassiveCouponApplication.java
@@ -0,0 +1,13 @@
+package com.jeondoh.massive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassiveCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MassiveCouponApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeondoh/massive/MassiveCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeondoh.massive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassiveCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
