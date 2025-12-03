aa914f73646451516a85c447f460d1334e84ec82
jeondoh <jundo1302@gmail.com>
2025-12-03 23:21:22 +0900

    feat: 인메모리 기반 저장소 전용 어노테이션 추가

---
+++ queue-core/src/main/java/com/jeondoh/queuecore/annotation/Structure.java
@@ -0,0 +1,15 @@
+package com.jeondoh.queuecore.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface Structure {
+    @AliasFor(annotation = Component.class)
+    String value() default "";
+}
