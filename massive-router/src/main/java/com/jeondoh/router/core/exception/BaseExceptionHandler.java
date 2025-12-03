31be2bbd2c6e91123b31edf5e93e34a2434027b0
jeondoh <jundo1302@gmail.com>
2025-12-03 14:09:26 +0900

    feat: RestControllerAdvice

---
+++ massive-router/src/main/java/com/jeondoh/router/core/exception/BaseExceptionHandler.java
@@ -0,0 +1,80 @@
+package com.jeondoh.router.core.exception;
+
+import com.jeondoh.core.common.exception.BaseException;
+import com.jeondoh.core.reactive.ResponseApi;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestControllerAdvice
+public class BaseExceptionHandler {
+
+    /**
+     * 도메인 예외 처리
+     */
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ResponseApi<Object>> handleBaseException(BaseException ex) throws URISyntaxException {
+        String code = ex.getCode();
+        String message = ex.getMessage();
+        String detailMessage = ex.getDetailMessage();
+        HttpStatusCode statusCode = ex.getStatusCode();
+
+        if (statusCode.is3xxRedirection()) {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setLocation(new URI(ex.getRedirectUrl()));
+            return new ResponseEntity<>(httpHeaders, HttpStatus.PERMANENT_REDIRECT);
+        }
+
+        String finalMessage = detailMessage != null ? message + " " + detailMessage : message;
+        ResponseApi<Object> body = ResponseApi.nok(statusCode, code, finalMessage);
+
+        log.error("{}: {}", statusCode, finalMessage);
+
+        return ResponseEntity.status(statusCode).body(body);
+    }
+
+    /**
+     * @Valid로 인한 예외 처리
+     */
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @ExceptionHandler(WebExchangeBindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseApi<Object> handleWebExchangeBindException(WebExchangeBindException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(e -> e.getField() + ": " + e.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        log.error("{}: {}", HttpStatus.BAD_REQUEST, errorMessage);
+
+        return ResponseApi.nok(HttpStatus.BAD_REQUEST, "VALIDATION_FAILED", errorMessage);
+    }
+
+    /**
+     * 기타 서버 예외처리
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseApi<Object> handleInternalServerException(Exception ex) {
+        String errorMessage = "서버 내부 오류가 발생하였습니다.";
+        log.error(errorMessage, ex);
+
+        return ResponseApi.nok(HttpStatus.INTERNAL_SERVER_ERROR, "INTERNAL_SERVER_ERROR", errorMessage);
+    }
+
+}
