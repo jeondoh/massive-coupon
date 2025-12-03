ee2cf675aa2b7b57d987b89b2d69bfb572b35535
jeondoh <jundo1302@gmail.com>
2025-12-03 14:09:15 +0900

    feat: reactive redis bean

---
+++ massive-router/src/main/java/com/jeondoh/router/core/config/ReactiveRedisConfig.java
@@ -0,0 +1,51 @@
+package com.jeondoh.router.core.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class ReactiveRedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.database}")
+    private int database;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Bean
+    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate() {
+        StringRedisSerializer serializer = new StringRedisSerializer();
+        RedisSerializationContext<String, String> serializationContext = RedisSerializationContext
+                .<String, String>newSerializationContext()
+                .key(serializer)
+                .value(serializer)
+                .hashKey(serializer)
+                .hashValue(serializer)
+                .build();
+
+        return new ReactiveRedisTemplate<>(reactiveRedisConnectionFactory(), serializationContext);
+    }
+
+    @Bean
+    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        configuration.setPassword(password);
+        configuration.setDatabase(database);
+        return new LettuceConnectionFactory(configuration);
+    }
+}
