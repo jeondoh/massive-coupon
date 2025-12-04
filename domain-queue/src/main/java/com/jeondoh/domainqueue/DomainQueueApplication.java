5daf7f99657fcd66a9dbb5ed444592f6c3ea542a
jeondoh <jundo1302@gmail.com>
2025-12-04 13:33:21 +0900

    feat: domain-queue 모듈 추가
    대기열 큐 서버

---
+++ domain-queue/src/main/java/com/jeondoh/domainqueue/DomainQueueApplication.java
@@ -0,0 +1,18 @@
+package com.jeondoh.domainqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {
+        "com.jeondoh.domainqueue",
+        "com.jeondoh.core.common.component",
+        "com.jeondoh.core.servlet.component",
+        "com.jeondoh.queuecore.component"
+})
+public class DomainQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainQueueApplication.class, args);
+    }
+
+}
---
+++ domain-queue/src/test/java/com/jeondoh/domainqueue/DomainQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeondoh.domainqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
