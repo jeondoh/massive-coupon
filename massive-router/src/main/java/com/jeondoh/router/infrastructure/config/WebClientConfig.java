db79b50cd90d4b7f71d240bc16cef109ca4593fe
jeondoh <jundo1302@gmail.com>
2025-12-05 16:30:50 +0900

    fix: webclient bean 옵션 추가

--- massive-router/src/main/java/com/jeondoh/router/infrastructure/config/WebClientConfig.java
+++ massive-router/src/main/java/com/jeondoh/router/infrastructure/config/WebClientConfig.java
@@ -1,14 +1,35 @@
 package com.jeondoh.router.infrastructure.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
+    @Value("${domain.server.timeout-seconds}")
+    private int timeoutSeconds;
+
     @Bean
     public WebClient webClient() {
-        return WebClient.builder().build();
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, timeoutSeconds * 1000)
+                .responseTimeout(Duration.ofSeconds(timeoutSeconds))
+                .doOnConnected(conn -> {
+                    conn.addHandlerFirst(new ReadTimeoutHandler(timeoutSeconds))
+                            .addHandlerLast(new WriteTimeoutHandler(timeoutSeconds));
+                });
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
     }
 }
