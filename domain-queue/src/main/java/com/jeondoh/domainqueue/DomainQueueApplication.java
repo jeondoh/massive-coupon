95c4901e108da0f53b711c1f189673f7608951b3
jeondoh <jundo1302@gmail.com>
2025-12-08 18:03:12 +0900

    featL EnableScheduling

--- domain-queue/src/main/java/com/jeondoh/domainqueue/DomainQueueApplication.java
+++ domain-queue/src/main/java/com/jeondoh/domainqueue/DomainQueueApplication.java
@@ -2,7 +2,9 @@ package com.jeondoh.domainqueue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {
         "com.jeondoh.domainqueue",
         "com.jeondoh.core.common.component",
