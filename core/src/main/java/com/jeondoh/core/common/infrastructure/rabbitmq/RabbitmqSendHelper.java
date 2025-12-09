d3d74f5a5178c0867b879f34c56dc8fe6c3b5a34
jeondoh <jundo1302@gmail.com>
2025-12-10 01:48:53 +0900

    feat: RabbitMQ 전송 전용 Helper 클래스

---
+++ core/src/main/java/com/jeondoh/core/common/infrastructure/rabbitmq/RabbitmqSendHelper.java
@@ -0,0 +1,25 @@
+package com.jeondoh.core.common.infrastructure.rabbitmq;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RabbitmqSendHelper {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    // 메세지 전송
+    public <T> void sendMessage(String exchange, String routingKey, T message) {
+        try {
+            rabbitTemplate.convertAndSend(exchange, routingKey, message);
+        } catch (AmqpException e) {
+            log.error("[RabbitMQ] sendMessage 실패 exchange: {}, routingKey: {}", exchange, routingKey);
+            log.error(e.getMessage(), e);
+        }
+    }
+}
