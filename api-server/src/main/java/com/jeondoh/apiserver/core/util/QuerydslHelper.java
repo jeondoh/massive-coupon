b2425fde4b82f6b5126f6a924255f41820c20228
jeondoh <jundo1302@gmail.com>
2025-12-09 16:43:17 +0900

    feat: Querydsl 설정

---
+++ api-server/src/main/java/com/jeondoh/apiserver/core/util/QuerydslHelper.java
@@ -0,0 +1,19 @@
+package com.jeondoh.apiserver.core.util;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+
+import java.util.function.Supplier;
+
+public class QuerydslHelper {
+
+    // NPE 방지
+    public static BooleanBuilder nullSafeBuilder(Supplier<BooleanExpression> f) {
+        try {
+            return new BooleanBuilder(f.get());
+        } catch (IllegalArgumentException e) {
+            return new BooleanBuilder();
+        }
+    }
+
+}
