7d8d4cad926efa869737544a19cf1a8a3dda5878
jeondoh <jundo1302@gmail.com>
2026-01-01 23:00:29 +0900

    feat: MQ 커스텀 예외 추가

--- core/src/main/java/com/jeondoh/core/common/exception/BaseCoreException.java
+++ core/src/main/java/com/jeondoh/core/common/exception/BaseCoreException.java
@@ -9,6 +9,14 @@ public class BaseCoreException extends BaseException {
     }
 
     public static BaseCoreException luaFileIoException() {
-        return new BaseCoreException("CORE_LUA_IO", "LUA IO 파일 오류", HttpStatus.INTERNAL_SERVER_ERROR);
+        return new BaseCoreException("BASE_01", "LUA IO 파일 오류", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public static BaseCoreException amqpProduceException() {
+        return new BaseCoreException("BASE_02", "MQ Produce 오류", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public static BaseCoreException amqpConsumeException() {
+        return new BaseCoreException("BASE_03", "MQ Consume 오류", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
