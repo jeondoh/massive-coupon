700c7819580380b24565488dbea165608fca7067
jeondoh <jundo1302@gmail.com>
2026-01-01 23:01:26 +0900

    fix: 공용 consumer 적용

--- domain-queue/src/main/java/com/jeondoh/domainqueue/infrastructure/rabbitmq/CouponQueueRemoveConsumer.java
+++ domain-queue/src/main/java/com/jeondoh/domainqueue/infrastructure/rabbitmq/CouponQueueRemoveConsumer.java
@@ -1,20 +1,23 @@
 package com.jeondoh.domainqueue.infrastructure.rabbitmq;
 
 import com.jeondoh.core.common.dto.coupon.CouponIssuedRemoveAtRunningQueueMessage;
+import com.jeondoh.core.common.infrastructure.rabbitmq.RabbitMQConsumer;
 import com.jeondoh.domainqueue.domain.service.QueueRunningService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class CouponQueueRemoveListener {
+public class CouponQueueRemoveConsumer {
 
+    private final RabbitMQConsumer rabbitMQConsumer;
     private final QueueRunningService queueRunningService;
 
     @RabbitListener(queues = "${spring.rabbitmq.queues.coupon-queue-remove.queue-name}")
-    public void handleCouponRemove(CouponIssuedRemoveAtRunningQueueMessage message) {
-        queueRunningService.removeRunningQueueMember(message);
+    public void handleCouponRemove(@Payload CouponIssuedRemoveAtRunningQueueMessage message) {
+        rabbitMQConsumer.consume(message, queueRunningService::removeRunningQueueMember);
     }
 
 }
