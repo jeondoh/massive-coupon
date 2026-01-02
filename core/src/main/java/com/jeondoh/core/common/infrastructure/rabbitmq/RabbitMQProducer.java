5ba17280461069931dc539ab6ac27b8e7d92319a
jeondoh <jundo1302@gmail.com>
2026-01-02 21:03:23 +0900

    fix: component 제거, 공용 클래스 변수 추가

--- core/src/main/java/com/jeondoh/core/common/infrastructure/rabbitmq/RabbitMQProducer.java
+++ core/src/main/java/com/jeondoh/core/common/infrastructure/rabbitmq/RabbitMQProducer.java
@@ -1,25 +1,29 @@
 package com.jeondoh.core.common.infrastructure.rabbitmq;
 
 import com.jeondoh.core.common.exception.BaseCoreException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
-@RequiredArgsConstructor
-public class RabbitMQProducer {
+public abstract class RabbitMQProducer {
 
-    private final RabbitTemplate rabbitTemplate;
+    protected final RabbitTemplate rabbitTemplate;
+    protected final String exchangeName;
+    protected final String routingKey;
+
+    protected RabbitMQProducer(RabbitTemplate rabbitTemplate, String exchangeName, String routingKey) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.exchangeName = exchangeName;
+        this.routingKey = routingKey;
+    }
 
     // 메세지 전송
-    public <T> void sendMessage(String exchange, String routingKey, T message) {
+    public <T> void sendMessage(T message) {
         try {
-            rabbitTemplate.convertAndSend(exchange, routingKey, message);
+            rabbitTemplate.convertAndSend(exchangeName, routingKey, message);
         } catch (AmqpException e) {
-            log.error("[RabbitMQ] sendMessage 실패 exchange: {}, routingKey: {}", exchange, routingKey);
+            log.error("[RabbitMQ] sendMessage 실패 exchange: {}, routingKey: {}", exchangeName, routingKey);
             log.error(e.getMessage(), e);
             throw BaseCoreException.amqpProduceException();
         }
